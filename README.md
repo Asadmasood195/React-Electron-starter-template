ddb31edb2d319643d35fa3971080852b997bc244
usama-govsoft <robert@govsoft.us>
2021-06-11 12:38:27 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<img src="./public/images/Annotation 2021-06-10 210014.png">
---
+++ src/react/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to React Electron</h3>
+    </div>
+  )
+}
+
+export default App
